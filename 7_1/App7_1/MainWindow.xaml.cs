f17154fbe98c97eaee8d38ac3525e562753b97a6
laiiihz <laiiihz@outlook.com>
2018-10-19 13:45:48 +0800

    add answer of 7_1

---
+++ 7_1/App7_1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace App7_1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Data
+        {
+            public int min { get; set; }
+            public int max { get; set; }
+            public int avr { get; set; }
+        }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Text1.Text = "";
+            Data d = new Data(); 
+            d.avr = 0;
+            Random random = new Random((int)DateTime.Now.Millisecond);
+            int[] a = new int[20];
+            for (int i = 0; i < 20; i++)
+            {
+                a[i] = random.Next(0, 501);
+                Text1.Text = Text1.Text + " " + a[i];
+                d.avr += a[i];
+            }
+            ArrayList list = new ArrayList(a);
+            list.Sort();
+            d.min = (int)list[0];
+            d.max = (int)list[19];
+            d.avr = d.avr / 20;
+            text_min.DataContext = d;
+            text_avr.DataContext = d;
+            text_max.DataContext = d;
+        }
+    }
+}
